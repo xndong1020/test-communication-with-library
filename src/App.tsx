db44c63c6f0654334e03880a1148215b0d3ac958
Jeremy Gu <jgu@apra.com.au>
2022-06-17 16:30:37 +1000

    init setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import {
+  globalObservable,
+  Button,
+  Input,
+} from "nicole-test-components-library";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    console.log("aaa");
+    globalObservable.publish("Message 1111");
+  }, []);
+
+  const handleNewMessage = (newMessage: string) => {
+    // setMessages((currentMessages) => currentMessages.concat(newMessage));
+    alert(`Paren received new message from child: ${newMessage}`);
+  };
+
+  useEffect(() => {
+    console.log("Start subscribe");
+    globalObservable.subscribe(handleNewMessage);
+    return () => {
+      globalObservable.unsubscribe(handleNewMessage);
+    };
+  }, [handleNewMessage]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button />
       </header>
     </div>
   );
